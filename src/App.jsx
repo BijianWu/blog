65b9dd5cb7575a6e8ff856235d45536d163b6120
BijianWu <384318185@qq.com>
2023-11-02 01:08:25 +0000

    chore: example use of the Test ReusableButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Button, Container } from "@mui/material";
 import React, { useState } from "react";
 import BlogDetails from "./blog/BlogDetails";
+import TestReusableButton from "./components/TestReusableButton";
 
 
 export default function App(){
 
     return <>
         <Container maxWidth="md">
-            <BlogDetails />
+            {/* <BlogDetails /> */}
+            <TestReusableButton sx={{p:3} } disableRipple> Button</TestReusableButton>
         </Container>
 
         {/* <h1>hi there</h1> */}
