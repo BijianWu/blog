38f25f48101505ea6f0f2f322eb3546152244ec4
BijianWu <384318185@qq.com>
2023-11-23 23:37:38 +0000

    create tabs page

--- src/pages/Tags.jsx
+++ src/pages/Tags.jsx
@@ -1,6 +1,31 @@
+import { Chip, Grid, Link } from "@mui/material";
+import StyledChip from "../components/StyledChip";
+
 export default function Tags(){
 
     return <>
-        Tags Page
+        <Grid container rowGap={2} columnGap={3}>
+            <Grid item lg={3} xs={5}>
+                <StyledChip label="Game Development"  />
+            </Grid>
+
+            <Grid item lg={3} xs={5}>
+                <StyledChip label="Frontend Development" />
+            </Grid>
+
+            <Grid item lg={3} xs={5}>
+                <StyledChip label="Backend Development" />
+            </Grid>
+
+            <Grid item lg={3} xs={5}>
+                <StyledChip label="Programming Concept" />
+            </Grid>
+
+            <Grid item lg={3} xs={5}>
+                <StyledChip label="Programming Technique" />
+            </Grid>
+
+        </Grid>
+
     </>
 }
