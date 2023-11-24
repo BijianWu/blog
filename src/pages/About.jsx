96b7d39ba2aa63b4469b1bb6918ea04abd03e1ec
BijianWu <384318185@qq.com>
2023-11-24 00:00:47 +0000

    simple about and contact me without much styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,15 @@
+import { Typography } from "@mui/material";
+
 export default function About(){
 
     return <>
-        About Page
+        <Typography variant="h5" lineHeight={2}>About</Typography>
+
+        <Typography variant="h6" lineHeight={2}>Hi, I am Bijian</Typography>
+
+        <Typography paragraph lineHeight={2}>
+        I am proficient in different programming languages and APIs, have acquired relevant experience in software development using Visual Studio particularly C#, front-end web development using JavaScript, back-end development using Java, JavaScript, PHP and Python, game development in Unity and Unreal, have in-depth knowlege of MySql and MongoDb, and also have profound experiences in containerisation tools like Docker and Kubernetes.
+I am a very enthusiastic and hardworking individual dedicated to high quality work produced timely meeting given deadlines and targets. In my free time I am always developing my skills, practising new ways of overcoming given tasks speedily and learning new related skills using online resources and other sources provided or offered at programming events.
+        </Typography>
     </>
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,14 @@
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
 export default function Contact(){
 
     return <>
-        Contact Page
+        <Typography variant="h5" lineHeight={2}>Conact</Typography>
+
+        <Typography variant="h6" lineHeight={2}>Email: bijian1@hotmail.com</Typography>
+
+        <Typography variant="h6" lineHeight={2}>LinkedIn: <Link to="https://www.linkedin.com/in/bijian-wu-4b01ba176/">https://www.linkedin.com/in/bijian-wu-4b01ba176/</Link></Typography>
+        <Typography variant="h6" lineHeight={2}>Portfolio: <Link to="https://bijianwu.github.io/portfolio/">https://bijianwu.github.io/portfolio/</Link></Typography>
     </>
 }
