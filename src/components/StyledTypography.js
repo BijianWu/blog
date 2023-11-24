e20b61545f159c0fc80208172975660ddb2f1f85
BijianWu <384318185@qq.com>
2023-11-24 00:51:46 +0000

    tag details page reformat

---
+++ src/components/StyledTypography.js
@@ -0,0 +1,31 @@
+import { Chip, Tab, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+const StyledTypography = styled((props) => <Typography variant="outlined" size="small" sx={{ fontSize: 20 }} clickable {...props} />)(({ theme }) => ({
+    fontWeight: theme.typography.fontWeightRegular,
+    marginRight: theme.spacing(1),
+    color: '#1976d2',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      color: "#ECAF72",
+      opacity: 1,
+      borderBottom: "1px solid #ECAF72",
+      paddingBottom: "5px"
+    },
+    '&.Mui-focusVisible': {
+      backgroundColor: '#d1eaff',
+    },
+  }));
+
+export default StyledTypography;
--- src/pages/TagDetails.jsx
+++ src/pages/TagDetails.jsx
@@ -1,6 +1,7 @@
-import { Chip, Grid, Link, Typography } from "@mui/material";
+import { Chip, Grid, Link, List, ListItem, Typography } from "@mui/material";
 import StyledChip from "../components/StyledChip";
 import { useParams } from "react-router-dom";
+import StyledTypography from "../components/StyledTypography";
 
 export default function TagDetails(){
     const { tagName } = useParams();
@@ -8,6 +9,11 @@ export default function TagDetails(){
     const convertedTagName = replaceDashTagName.charAt(0).toUpperCase() + replaceDashTagName.slice(1)
     return <>
         <Typography>{convertedTagName}</Typography>
-
+        <List sx={{ listStyleType: 'disc' }}>
+            <ListItem sx={{ display: 'list-item' }} >
+                <StyledTypography>good article 2</StyledTypography>............ Nov 21, 2022
+                </ListItem>
+            <ListItem sx={{ display: 'list-item' }}><StyledTypography>good article 1</StyledTypography> ............ Nov 20, 2022</ListItem>
+        </List>
     </>
 }
