2588ec3cb681c9f29f5b431b5b5546a68978b8bf
BijianWu <384318185@qq.com>
2023-11-26 01:29:40 +0000

    style the aboutme and the Contact page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,29 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
+import StyledPageHeadingTypography from "../components/StyledPageHeadingTypography";
 
 export default function About(){
 
     return <>
-        <Typography variant="h5" lineHeight={2}>About</Typography>
+        <StyledPageHeadingTypography>About</StyledPageHeadingTypography>
+        <Stack spacing={2}>
+            <Typography>Hi, I am Bijian</Typography>
 
-        <Typography variant="h6" lineHeight={2}>Hi, I am Bijian</Typography>
+            <Typography>
+            I am proficient in different programming languages and APIs, have acquired relevant experience in software development using Visual Studio particularly C#, front-end web development using JavaScript, back-end development using Java, JavaScript, PHP and Python, game development in Unity and Unreal, 
+            </Typography>
 
-        <Typography paragraph lineHeight={2}>
-        I am proficient in different programming languages and APIs, have acquired relevant experience in software development using Visual Studio particularly C#, front-end web development using JavaScript, back-end development using Java, JavaScript, PHP and Python, game development in Unity and Unreal, have in-depth knowlege of MySql and MongoDb, and also have profound experiences in containerisation tools like Docker and Kubernetes.
-I am a very enthusiastic and hardworking individual dedicated to high quality work produced timely meeting given deadlines and targets. In my free time I am always developing my skills, practising new ways of overcoming given tasks speedily and learning new related skills using online resources and other sources provided or offered at programming events.
-        </Typography>
+            <Typography>
+            have in-depth knowlege of MySql and MongoDb
+            </Typography>
+
+            <Typography>
+            also have profound experiences in containerisation tools like Docker and Kubernetes.
+            </Typography>
+
+            <Typography>
+            I am a very enthusiastic and hardworking individual dedicated to high quality work produced timely meeting given deadlines and targets. In my free time I am always developing my skills, practising new ways of overcoming given tasks speedily and learning new related skills using online resources and other sources provided or offered at programming events.
+            </Typography>
+        </Stack>
+
     </>
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,33 @@
-import { Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import StyledTypography from "../components/StyledTypography";
+import StyledLinkTypography from "../components/StyledLinkTypography";
+import StyledPageHeadingTypography from "../components/StyledPageHeadingTypography";
 
 export default function Contact(){
 
     return <>
-        <Typography variant="h5" lineHeight={2}>Conact</Typography>
+        <StyledPageHeadingTypography>Conact</StyledPageHeadingTypography>
 
-        <Typography variant="h6" lineHeight={2}>Email: bijian1@hotmail.com</Typography>
+        <Stack spacing={2}>
+            <Stack direction={"row"}>
+                <Typography  sx={{mr: 2}} >Email:</Typography>
+                <StyledLinkTypography >bijian1@hotmail.com</StyledLinkTypography>
+            </Stack>
 
-        <Typography variant="h6" lineHeight={2}>LinkedIn: <Link to="https://www.linkedin.com/in/bijian-wu-4b01ba176/">https://www.linkedin.com/in/bijian-wu-4b01ba176/</Link></Typography>
-        <Typography variant="h6" lineHeight={2}>Portfolio: <Link to="https://bijianwu.github.io/portfolio/">https://bijianwu.github.io/portfolio/</Link></Typography>
+            <Stack direction={"row"}>
+                <Typography  sx={{mr: 2}} >LinkedIn:</Typography>
+                <StyledLinkTypography >https://www.linkedin.com/in/bijian-wu-4b01ba176</StyledLinkTypography>
+            </Stack>
+
+            <Stack direction={"row"}>
+                <Typography  sx={{mr: 2}} >Portfolio:</Typography>
+                <StyledLinkTypography >https://bijianwu.github.io/portfolio</StyledLinkTypography>
+            </Stack>
+        </Stack>
+
+{/* 
+        <Typography variant="body1" lineHeight={2}>LinkedIn: <Link to="https://www.linkedin.com/in/bijian-wu-4b01ba176/">https://www.linkedin.com/in/bijian-wu-4b01ba176/</Link></Typography>
+        <Typography variant="body1" lineHeight={2}>Portfolio: <Link to="https://bijianwu.github.io/portfolio/">https://bijianwu.github.io/portfolio/</Link></Typography> */}
     </>
 }
