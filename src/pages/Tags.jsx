112c58111badf3f3831492afbf11b166253a1e7a
BijianWu <384318185@qq.com>
2023-11-26 00:41:43 +0000

    show counters of how many elements were reach tags has

--- src/pages/Tags.jsx
+++ src/pages/Tags.jsx
@@ -1,6 +1,7 @@
-import { Chip, Grid, Link } from "@mui/material";
+import { Badge, Box, Chip, Grid, Link, Stack, Typography } from "@mui/material";
 import StyledChip from "../components/StyledChip";
 import { useNavigate } from "react-router-dom";
+import MutedTypography from "../components/MutedTypography";
 
 export default function Tags(){
     const navigate = useNavigate();
@@ -8,23 +9,44 @@ export default function Tags(){
     return <>
         <Grid container rowGap={2} columnGap={3}>
             <Grid item lg={3} xs={5}>
-                <StyledChip label="Game Development" onClick= { () => navigate("game-development") } />
+                <StyledChip label={
+                    <Stack direction={"row"}>
+                        Game Development <MutedTypography>1</MutedTypography>
+                    </Stack>
+                    
+                } onClick= { () => navigate("game-development") } />
             </Grid>
 
             <Grid item lg={3} xs={5}>
-                <StyledChip label="Frontend Development" onClick= { () => navigate("frontend-development") } />
+                <StyledChip label={
+                    <Stack direction={"row"}>
+                        Frontend development <MutedTypography>1</MutedTypography>
+                    </Stack>
+                } onClick= { () => navigate("frontend-development") } />
             </Grid>
 
             <Grid item lg={3} xs={5}>
-                <StyledChip label="Backend Development" onClick= { () => navigate("backend-development") } />
+                <StyledChip label={
+                    <Stack direction={"row"}>
+                        Backend development <MutedTypography>4</MutedTypography>
+                    </Stack>
+                } onClick= { () => navigate("backend-development") } />
             </Grid>
 
             <Grid item lg={3} xs={5}>
-                <StyledChip label="Programming Concept" onClick= { () => navigate("programming-concept") } />
+                <StyledChip label={
+                    <Stack direction={"row"}>
+                        Programming concept <MutedTypography>3</MutedTypography>
+                    </Stack>
+                } onClick= { () => navigate("programming-concept") } />
             </Grid>
 
             <Grid item lg={3} xs={5}>
-                <StyledChip label="Programming Technique" onClick= { () => navigate("programming-technique") } />
+                <StyledChip label={
+                    <Stack direction={"row"}>
+                        Programming technique <MutedTypography>2</MutedTypography>
+                    </Stack>
+                } onClick= { () => navigate("programming-technique") } />
             </Grid>
 
         </Grid>
