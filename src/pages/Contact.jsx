c84b6d5e2f9c9e214a0479aae04e99f00e650010
BijianWu <384318185@qq.com>
2023-11-28 00:55:55 +0000

    wraps to a new line for Contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,30 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack, Typography, useMediaQuery } from "@mui/material";
 import { Link } from "react-router-dom";
 import StyledTypography from "../components/StyledTypography";
 import StyledLinkTypography from "../components/StyledLinkTypography";
 import StyledPageHeadingTypography from "../components/StyledPageHeadingTypography";
 import { openInMailApp, openInNewTab } from "../utils/navUtils";
+import { useTheme } from "@emotion/react";
 
 export default function Contact(){
-
+    const theme = useTheme();
+    const mdMatches = useMediaQuery(theme.breakpoints.up('md'));
     const noWrapTextStyle = { textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"  };
     return <>
         <StyledPageHeadingTypography>Conact</StyledPageHeadingTypography>
 
         <Stack spacing={3}>
-            <Stack direction={"row"}>
+            <Stack direction={mdMatches ? "row" : "column"} alignItems={"flex-start"}>
                 <Typography  sx={{mr: 2}} >Email:</Typography>
                 <StyledLinkTypography onClick={ () => openInMailApp("bijian1@hotmail.com")} sx={noWrapTextStyle}>bijian1@hotmail.com</StyledLinkTypography>
             </Stack>
 
-            <Stack direction={"row"}>
+            <Stack direction={mdMatches ? "row" : "column"} alignItems={"flex-start"}>
                 <Typography  sx={{mr: 2}} >LinkedIn:</Typography>
                 <StyledLinkTypography onClick={ () => openInNewTab("https://www.linkedin.com/in/bijian-wu-4b01ba176")} sx={noWrapTextStyle}>https://www.linkedin.com/in/bijian-wu-4b01ba176</StyledLinkTypography>
             </Stack>
 
-            <Stack direction={"row"}>
+            <Stack direction={mdMatches ? "row" : "column"} alignItems={"flex-start"}>
                 <Typography  sx={{mr: 2}} >Portfolio:</Typography>
                 <StyledLinkTypography onClick={ () => openInNewTab("https://bijianwu.github.io/portfolio/")} sx={noWrapTextStyle}>https://bijianwu.github.io/portfolio</StyledLinkTypography>
             </Stack>
