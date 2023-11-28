b30113f9eb95779491be566b94750d2f25331e01
BijianWu <384318185@qq.com>
2023-11-28 14:46:28 +0000

    blog posts layout

--- src/blog/BlogPosts.jsx
+++ src/blog/BlogPosts.jsx
@@ -1,17 +1,23 @@
-import { Button, Container, Grid, ThemeProvider } from "@mui/material";
+import { Button, Container, Grid, ThemeProvider, useMediaQuery, useTheme } from "@mui/material";
 import React, { useState } from "react";
 
 import BlogPost from "./BlogPost";
 import BridgeDesignPattern from "../pages/blogs/november_28_2023/BridgeDesignPatternBlogDetails";
 import BridgeDesignPatternBlogPost from "../pages/blogs/november_28_2023/BridgeDesignPatternBlogPost";
 
-
-
 export default function BlogPosts(){
+    const theme = useTheme();
+    const mdMatches = useMediaQuery(theme.breakpoints.up('md'));
 
     return <>
-    <Grid container spacing={5}>
-        <Grid item xl={4} md={6} xs={12} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+    <Grid container spacing={mdMatches ? 10 : 5}>
+        <Grid item xl={4} md={6} xs={12} display={mdMatches ? "" : "flex"} justifyContent={"center"} alignItems={"center"}>
+            <BridgeDesignPatternBlogPost />
+        </Grid>
+        <Grid item xl={4} md={6} xs={12} display={mdMatches ? "" : "flex"} justifyContent={"center"} alignItems={"center"}>
+            <BridgeDesignPatternBlogPost />
+        </Grid>
+        <Grid item xl={4} md={6} xs={12} display={mdMatches ? "" : "flex"} justifyContent={"center"} alignItems={"center"}>
             <BridgeDesignPatternBlogPost />
         </Grid>
         {/* <Grid item xl={4} md={6} xs={12} display={"flex"} justifyContent={"center"} alignItems={"center"}>
