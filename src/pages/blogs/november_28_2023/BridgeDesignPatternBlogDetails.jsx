3c0487310e0d2c8cc0b2dbbfa310e05f7f18403b
BijianWu <384318185@qq.com>
2023-11-28 14:31:38 +0000

    put in imageUrl link address

--- src/pages/blogs/november_28_2023/BridgeDesignPatternBlogDetails.jsx
+++ src/pages/blogs/november_28_2023/BridgeDesignPatternBlogDetails.jsx
@@ -1,7 +1,7 @@
 import BlogDetails from "../../../blog/BlogDetails";
 
 export default function BridgeDesignPatternBlogDetails({imageUrl, title, dateString, mainContent }){
-
+    //https://pixabay.com/photos/bridge-park-garden-japanese-garden-53769/
     return <>
         <BlogDetails imageUrl={require("../../../assets/images/bridge.jpg")} title={"Bridge design pattern"} dateString={"Nov, 28, 2023"} mainContent={"bridge-design-pattern"}></BlogDetails>
     </>
