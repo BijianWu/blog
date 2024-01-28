b10a3920a9270bb91944f02ca43583ebb2e0768d
BijianWu <384318185@qq.com>
2024-01-28 01:18:24 +0000

    make blog details page smaller

--- src/pages/blogs/november_28_2023/BridgeDesignPatternBlogDetails.jsx
+++ src/pages/blogs/november_28_2023/BridgeDesignPatternBlogDetails.jsx
@@ -10,13 +10,13 @@ export default function BridgeDesignPatternBlogDetails(){
             mainContent={
             <>
                 <Typography variant="h6"  mb={1}>Overview</Typography>
-                <Typography paragraph mb={6}>
+                <Typography paragraph fontSize={"0.9rem"} mb={6}>
                     Bridge design pattern is designed to tacle the issue of monolithic god class that does too many things and are hard to make changes without breaking existing code,
                     the bridge design pattern helped to break the class into smaller classes that can be developed independently, to make the code more modular thus creating a better 
                     structural structure for the code base.
                 </Typography>
 
-                <Typography variant="h6"  mb={1}>Problem</Typography>
+                <Typography variant="h6" fontSize={"0.9rem"} mb={1}>Problem</Typography>
                 <Typography paragraph>
                     Let's say you are working on a game, where the game contains so many different types of enemies, flying type enemy, ground type enemy
                 </Typography>
--- src/pages/blogs/november_29_2023/SingleResponsibilityBlogDetails.jsx
+++ src/pages/blogs/november_29_2023/SingleResponsibilityBlogDetails.jsx
@@ -23,7 +23,7 @@ export default function SingleResponsibilityBlogDetails(){
             mainContent={
             <>
                 <Typography variant="h6"  mb={1}>Overview</Typography>
-                <Typography paragraph mb={6}>
+                <Typography paragraph fontSize={"0.9rem"} mb={6}>
                     Single Responsibitiy is the first principle from SOILD, where it sets a best practice for how programmer should code. A class or a functon must not have
                     different reasons to change, it should do one thing well. What the one thing is depends on the context of the application you are working, thus every time you see an if and
                     else statement that does not mean the function breaks the Single responsibility principle. when you see your function does multiple things, the best thing you do is
@@ -33,7 +33,7 @@ export default function SingleResponsibilityBlogDetails(){
                 </Typography>
 
                 <Typography variant="h6"  mb={1}>Example</Typography>
-                <Typography paragraph>
+                <Typography paragraph fontSize={"0.9rem"}>
                     Let's say you are working on a game, where the game contains so many different types of enemies, flying type enemy, ground type enemy
                 </Typography>
 {/* 
