3642324d8379630ef3e0908f9968e424182dab92
BijianWu <384318185@qq.com>
2024-01-29 00:52:03 +0000

    basename also works

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <HashRouter>
+    <BrowserRouter basename="/blog">
         <App />
-    </HashRouter>
+    </BrowserRouter>
 
 )
